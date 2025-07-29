f31fad6166a5f11d4789b91bb27636637bfd4be1
hajaejoon <gkwowns1@gmail.com>
2025-07-30 01:19:51 +0900

    feat: Firebase Crashlytics 추가

--- Health/Presentation/Home/HomeViewController.swift
+++ Health/Presentation/Home/HomeViewController.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+      super.viewDidLoad()
+
+      // Do any additional setup after loading the view, typically from a nib.
+
+      let button = UIButton(type: .roundedRect)
+      button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+      button.setTitle("Test Crash", for: [])
+      button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+      view.addSubview(button)
+  }
+
+  @IBAction func crashButtonTapped(_ sender: AnyObject) {
+      let numbers = [0]
+      let _ = numbers[1]
+  }
+}
