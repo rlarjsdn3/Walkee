1decaca3bde4706bd37dbeade8af7cdf8ac29606
김소월 <rlarjsdn3@naver.com>
2025-07-31 13:15:41 +0900

    feat: `StoryboardInstantiable` 및 `NSObject+Nib`, `NSObject+ID` 확장 코드 구현 (#10)
    * feat: GoogleCrashAnalytics 라이브러리 추가
    
    * feat: StoryboaardInstantiable, NSObject+ID, NSObject+Nib 확장 코드 구현

---
+++ Health/Core/Utils/NSObject+Nib.swift
@@ -0,0 +1,23 @@
+//
+//  NSObject+Nib.swift
+//  Health
+//
+//  Created by 김건우 on 7/28/25.
+//
+
+import UIKit
+
+@MainActor
+extension NSObject {
+
+    /// 클래스와 동일한 이름을 가진 Nib 파일을 로드합니다.
+    ///
+    /// - Returns: 현재 클래스 이름을 기준으로 생성된 `UINib` 인스턴스를 반환합니다.
+    ///            Nib 파일은 해당 클래스가 속한 번들에서 로드됩니다.
+    var nib: UINib {
+        UINib(
+            nibName: String(describing: Self.self),
+            bundle: Bundle(for: Self.self)
+        )
+    }
+}
--- Health/Core/Utils/UIImage+Resize.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UIImage+Resize.swift
-//  Health
-//
-//  Created by 김건우 on 7/28/25.
-//
-
-import Foundation
