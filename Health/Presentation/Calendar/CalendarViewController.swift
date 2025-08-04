1d0d3583fc9f044802ee926e7f5902aa24c4b0bb
Jongseong Kim <jseongee@gmail.com>
2025-08-04 16:24:18 +0900

    feat: 캘린더 ViewController 추가 및 Layout 설정 (#34)
    * feat: 캘린더 ViewController 추가 및 Layout 설정
    
    * refactor: reuseIdentifier 대신 id 사용
    
    * chore: "월별 기록" 폰트 수정
    
    * fix: 놓친 부분 수정

---
+++ Health/Presentation/Calendar/CalendarViewController.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+final class CalendarViewController: CoreViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func setupAttribute() {
+        super.setupAttribute()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.collectionViewLayout = createCompositionalLayout()
+        collectionView.register(CalendarMonthCell.self, forCellWithReuseIdentifier: CalendarMonthCell.id)
+        collectionView.backgroundColor = .systemBackground
+    }
+
+    private func createCompositionalLayout() -> UICollectionViewLayout {
+        UICollectionViewCompositionalLayout { sectionIndex, environment in
+            let itemSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .estimated(44)
+            )
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+            let groupSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .estimated(300)
+            )
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+
+            let section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = 16
+            section.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16)
+            return section
+        }
+    }
+}
+
+extension CalendarViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 12
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CalendarMonthCell.id, for: indexPath) as? CalendarMonthCell else {
+            fatalError("Failed to dequeue CalendarMonthCell")
+        }
+        // TODO: month 데이터 주입 예정
+        return cell
+    }
+}
---
+++ Health/Presentation/Calendar/Views/CalendarMonthCell.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+final class CalendarMonthCell: CoreCollectionViewCell {
+    
+}
