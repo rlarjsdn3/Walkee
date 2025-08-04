a4e8acce421986b5bf8b35e9d354312d1f44e363
giseungNoh <juks8666@gmail.com>
2025-08-04 16:27:06 +0900

    충돌 처리 (#35)

---
+++ Health/Presentation/Personal/Views/ChatBotView/ChatbotViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChatbotViewController.swift
+//  Health
+//
+//  Created by juks86 on 8/4/25.
+//
+
+import UIKit
+
+class ChatbotViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
