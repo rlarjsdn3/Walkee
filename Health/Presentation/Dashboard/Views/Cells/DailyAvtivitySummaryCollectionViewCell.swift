e4bb2fd1cd37d8a78a0abccc662f2e9d4ea793fb
rlarjsdn3 <rlarjsdn3@naver.com>
2025-08-05 12:58:51 +0900

    feat: DailyActivitySummaryCell, DailyGaolRingCell 파일 생성

---
+++ Health/Presentation/Dashboard/Views/Cells/DailyAvtivitySummaryCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  DailyAvtivitySummaryCollectionViewCell.swift
+//  Health
+//
+//  Created by 김건우 on 8/5/25.
+//
+
+import UIKit
+
+final class DailyAvtivitySummaryCollectionViewCell: CoreCollectionViewCell {
+
+    override func setupAttribute() {
+        backgroundColor = .secondarySystemBackground
+    }
+}
