1af99785c4b7955724c231dfa196a614df4ddd2f
Seohyun Kim <cestbonciel88@knou.ac.kr>
2025-08-05 18:39:36 +0900

    design: xib 생성 및 ib에서 제약과 관련된 설정

---
+++ Health/Presentation/Chatbot/Views/Cells/BubbleViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BubbleViewCell.swift
+//  Health
+//
+//  Created by Nat Kim on 8/5/25.
+//
+
+import UIKit
+
+class BubbleViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
