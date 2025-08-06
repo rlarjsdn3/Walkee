397dadd0e679a8fd3e42ef7a98b14145eb10711c
rlarjsdn3 <rlarjsdn3@naver.com>
2025-08-06 14:12:03 +0900

    feat: LineChartsView, LineChartsHostingController 코드 구현

---
+++ Health/Presentation/Dashboard/SwiftUI/LineChartsHostingController.swift
@@ -0,0 +1,24 @@
+//
+//  LineChartsHostingController.swift
+//  Health
+//
+//  Created by 김건우 on 8/6/25.
+//
+
+import SwiftUI
+import UIKit
+
+final class LineChartsHostingController: UIHostingController<LineChartsView> {
+
+    private(set) var chartsData: [ChartsData]
+
+    init(chartsData: [ChartsData]) {
+        self.chartsData = chartsData
+
+        super.init(rootView: LineChartsView(chartsData: chartsData))
+    }
+    
+    @MainActor @preconcurrency required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Health/Presentation/Dashboard/SwiftUI/Views/LineChartsView.swift
@@ -0,0 +1,48 @@
+//
+//  LineChartsView.swift
+//  Health
+//
+//  Created by 김건우 on 8/6/25.
+//
+
+import Charts
+import HealthKit
+import SwiftUI
+
+typealias ChartsData = (startDate: Date, endDate: Date, value: Double)
+
+struct LineChartsView: View {
+
+    private var chartsData: [ChartsData]
+
+    init(chartsData: [ChartsData]) {
+        self.chartsData = chartsData
+    }
+
+    var body: some View {
+        Chart(chartsData, id: \.endDate) { data in
+            LineMark(
+                x: .value("date", data.endDate.description),
+                y: .value("value", data.value)
+            )
+            .foregroundStyle(.accent)
+            .lineStyle(.init(lineWidth: 3.25))
+            .symbol(.circle)
+            .symbolSize(150)
+            .interpolationMethod(.catmullRom)
+        }
+        .chartXAxis(.hidden)
+        .chartYAxis(.hidden)
+    }
+}
+
+#Preview(traits: .fixedLayout(width: 300, height: 200)) {
+    let datas: [ChartsData] = (0..<7).map { index in
+        let date = Date.now.addingTimeInterval(TimeInterval(-index * 86_400))
+        let (startDay, endDay) = date.rangeOfDay()
+        return (startDay, endDay, Double.random(in: 0..<1000))
+    }
+
+    LineChartsView(chartsData: datas)
+        .padding()
+}
