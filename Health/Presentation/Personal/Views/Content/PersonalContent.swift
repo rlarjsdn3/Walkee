a6f51d0de09c1c3f692ae4c5fed6f93a798c20d2
giseungNoh <juks8666@gmail.com>
2025-08-06 15:05:12 +0900

    Feat: personal- 건강 데이터 요약 UI 구현 (#51)
    * UI작업중
    
    * 건강데이터 UI 1차 완성
    
    * 카드뷰 ConerLadius 적용
    
    * 네이밍 변경

---
+++ Health/Presentation/Personal/Views/Content/PersonalContent.swift
@@ -0,0 +1,114 @@
+//
+//  PersonalContent.swift
+//  Health
+//
+//  Created by juks86 on 8/5/25.
+//
+
+import UIKit
+
+enum PersonalContent {
+
+    // 섹션 정의
+    enum Section: Hashable {
+        case segment
+        case chart
+    }
+
+    // 아이템 정의
+    enum Item: Hashable {
+        case segmentControl
+        case chartData
+    }
+}
+
+@MainActor
+extension PersonalContent.Item {
+
+    /// 컬렉션 뷰 셀 dequeue
+    func dequeueReusableCollectionViewCell(
+        collectionView: UICollectionView,
+        segmentCellRegistration: UICollectionView.CellRegistration<AnalysisPeriodCell, Void>,
+        chartCellRegistration: UICollectionView.CellRegistration<UICollectionViewCell, Void>,
+        indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        switch self {
+        case .segmentControl:
+            return collectionView.dequeueConfiguredReusableCell(
+                using: segmentCellRegistration,
+                for: indexPath,
+                item: ()
+            )
+        case.chartData:
+            return collectionView.dequeueConfiguredReusableCell(
+                using: chartCellRegistration,
+                for: indexPath,
+                item: ()
+            )
+        }
+    }
+}
+
+@MainActor
+extension PersonalContent.Section {
+
+    /// 섹션에 맞는 레이아웃 빌드
+    func buildLayout(_ environment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection {
+        switch self {
+        case .segment:
+            return buildMainLayout(environment)
+        case .chart:
+            return buildChartLayout(environment)
+        }
+    }
+    /// 섹션 레이아웃 정의
+    private func buildMainLayout(_ environment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .estimated(100)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .estimated(100)
+        )
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(
+            top: 0,
+            leading: UICollectionViewConstant.defaultInset,
+            bottom: 0,
+            trailing: UICollectionViewConstant.defaultInset
+        )
+
+        return section
+    }
+
+    private func buildChartLayout(_ environment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .estimated(200)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .estimated(200)
+        )
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(
+            top: 0,
+            leading: UICollectionViewConstant.defaultInset,
+            bottom: 0,
+            trailing: UICollectionViewConstant.defaultInset
+        )
+        return section
+    }
+}
