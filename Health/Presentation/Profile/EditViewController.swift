3b862298411a35fb9ecdfe7d0edb386735a08c46
haejaejoon <63631258+haejaejoon@users.noreply.github.com>
2025-08-08 13:30:16 +0900

    feat: Profile 메인 뷰 UI 구현 (#65)

---
+++ Health/Presentation/Profile/EditViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EditViewController.swift
+//  Health
+//
+//  Created by 하재준 on 8/8/25.
+//
+
+import UIKit
+
+class EditViewController: CoreGradientViewController {
+
+    override func setupAttribute() {
+        super.setupAttribute()
+        
+        applySolidBackground(.midnightBlackBackground)
+    }
+    
+    
+}
