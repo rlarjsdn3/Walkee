bef00d2a1470f5bc314a3fab23c24ae9ec38b07c
Seohyun Kim <cestbonciel88@knou.ac.kr>
2025-08-10 15:38:16 +0900

    refactor: 공통 너비 계산기 적용

--- Health/Presentation/Chatbot/Views/Cells/AIResponseCell.swift
+++ Health/Presentation/Chatbot/Views/Cells/AIResponseCell.swift
@@ -15,7 +15,7 @@ class AIResponseCell: CoreTableViewCell {
 	
 	override func setupAttribute() {
 		super.setupAttribute()
-		
+
 		selectionStyle = .none
 		responseTextView.textContainer.lineFragmentPadding = 0
 		responseTextView.textContainerInset = .zero
@@ -25,24 +25,8 @@ class AIResponseCell: CoreTableViewCell {
 	
 	private func setupLeftAlignment() {
 		// 디바이스별 최대 너비 설정
-		let screenWidth = UIScreen.main.bounds.width
-		let maxTextWidth: CGFloat
-		
-		switch screenWidth {
-		case ...320: // iPhone SE 1세대
-			maxTextWidth = 240
-		case 321...375: // iPhone 8, SE 2세대
-			maxTextWidth = 280
-		case 376...414: // iPhone 8 Plus, 11
-			maxTextWidth = 320
-		case 415...: // iPhone 12 Pro Max 이상
-			maxTextWidth = 350
-		default:
-			maxTextWidth = min(280, screenWidth * 0.75)
-		}
-		
+		let maxTextWidth = ChatbotWidthCalculator.maxContentWidth(for: .aiResponseText)
 		textViewWidthConstraint.constant = maxTextWidth
-		
 		textViewTrailingConstraint.priority = .init(750)
 	}
 	
@@ -51,5 +35,4 @@ class AIResponseCell: CoreTableViewCell {
 		setNeedsLayout()
 		layoutIfNeeded()
 	}
-	
 }
