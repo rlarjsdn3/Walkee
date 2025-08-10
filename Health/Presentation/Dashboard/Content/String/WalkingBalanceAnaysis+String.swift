1a3322e45bb83a0aba742bb6e478da009b86c78a
rlarjsdn3 <rlarjsdn3@naver.com>
2025-08-10 10:22:04 +0900

    feat: 코드 리뷰 반영

--- Health/Presentation/Dashboard/Content/String/WalkingBalanceAnaysis+String.swift
+++ Health/Presentation/Dashboard/Content/String/WalkingBalanceAnaysis+String.swift
@@ -1,5 +1,5 @@
 //
-//  WalkingBalanceAnaysis+String.swift
+//  WalkingBalanceAnalysis+String.swift
 //  Health
 //
 //  Created by 김건우 on 8/8/25.
@@ -7,10 +7,10 @@
 
 import Foundation
 
-typealias WalkingBalanceAnaysisString = String.WalkingBalanceAnaysis
+typealias WalkingBalanceAnaysisString = String.WalkingBalanceAnalysis
 extension String {
 
-    struct WalkingBalanceAnaysis {
+    struct WalkingBalanceAnalysis {
         static let walkingSpeed = "보행 속도는 일정 시간 동안 걷는 거리로, 전반적인 이동 능력과 체력 수준을 보여줍니다. 속도가 지나치게 느리면 낙상 위험과 근감소증 가능성이 높아집니다."
         static let stepLength = "보행 보폭은 한 걸음의 길이를 의미하며, 균형과 근력의 지표입니다. 보폭이 짧아지면 보행 불안정과 낙상 위험이 증가할 수 있습니다."
         static let doubleSupportPercentage = "이중 지지 시간은 두 발이 동시에 지면에 닿아 있는 시간 비율로, 균형 유지 능력을 나타냅니다. 수치가 높으면 균형 감각 저하나 낙상 위험을 의심할 수 있습니다."
