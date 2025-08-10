d9468524eafc7b000aee51252313197c837cf9cc
rlarjsdn3 <rlarjsdn3@naver.com>
2025-08-10 20:33:30 +0900

    feat: DashboardCardType에서 thresholdValues도 가져올 수 있도록 코드 작성

--- Health/Presentation/Dashboard/Content/Enum/DashboardCardType.swift
+++ Health/Presentation/Dashboard/Content/Enum/DashboardCardType.swift
@@ -62,6 +62,9 @@ extension DashboardCardType {
         case .walkingDoubleSupportPercentage:   return .walkingDoubleSupportPercentage
         }
     }
+}
+
+extension DashboardCardType {
 
     ///
     func status(_ value: Double, age: Int) -> DashboardCardType.GaitStatus {
@@ -75,48 +78,108 @@ extension DashboardCardType {
 
     ///
     func status(forStepLength centi: Double, age: Int) -> DashboardCardType.GaitStatus {
-        switch age {
-        case (..<20):   return evaluateStatusGreaterThan(centi, normal: 65, caution: 55) // 10대
-        case (20..<30): return evaluateStatusGreaterThan(centi, normal: 65, caution: 55) // 20대
-        case (30..<40): return evaluateStatusGreaterThan(centi, normal: 63, caution: 53) // 30대
-        case (40..<50): return evaluateStatusGreaterThan(centi, normal: 60, caution: 50) // 40대
-        case (50..<60): return evaluateStatusGreaterThan(centi, normal: 58, caution: 48) // 50대
-        case (60..<70): return evaluateStatusGreaterThan(centi, normal: 55, caution: 45) // 60대
-        default: return evaluateStatusGreaterThan(centi, normal: 50, caution: 40)        // 70대
-        }
-    }
+        let thresholds: [Double] = stepLengthThresholdValues(age: age)
+        let normal = thresholds[1]  // 주어진 연령대에 해당하는 주의(Caution) 범위의 첫 번째 값 (lowerBound)
+        let caution = thresholds[2] // 주어진 연령대에 해당하는 주의(Caution) 범위의  마지막 값 (upperBound)
 
-    ///
-    func status(forAsymmetryPercentage percentage: Double, age: Int) -> DashboardCardType.GaitStatus {
-        evaluateStatusLessThan(percentage, normal: 0.10, caution: 0.19) // 전 연령 공통
+        return evaluateStatusGreaterThan(centi, normal: normal, caution: caution)
     }
 
     ///
     func status(forWalkingSpeed mps: Double, age: Int) -> DashboardCardType.GaitStatus {
-        switch age {
-        case (..<20):   return evaluateStatusGreaterThan(mps, normal: 1.4, caution: 1.2)   // 10대
-        case (20..<30): return evaluateStatusGreaterThan(mps, normal: 1.4, caution: 1.2)   // 20대
-        case (30..<40): return evaluateStatusGreaterThan(mps, normal: 1.35, caution: 1.15) // 30대
-        case (40..<50): return evaluateStatusGreaterThan(mps, normal: 1.3, caution: 1.1)   // 40대
-        case (50..<60): return evaluateStatusGreaterThan(mps, normal: 1.25, caution: 1.0)  // 50대
-        case (60..<70): return evaluateStatusGreaterThan(mps, normal: 1.1, caution: 0.9)   // 60대
-        default: return evaluateStatusGreaterThan(mps, normal: 1.0, caution: 0.8)          // 70대
-        }
+        let thresholds: [Double] = walkingSpeedThresholdValue(age: age)
+        let normal = thresholds[1]  // 주어진 연령대에 해당하는 주의(Caution) 범위의 첫 번째 값 (lowerBound)
+        let caution = thresholds[2] // 주어진 연령대에 해당하는 주의(Caution) 범위의  마지막 값 (upperBound)
+
+        return evaluateStatusGreaterThan(mps, normal: normal, caution: caution)
+    }
+
+    ///
+    func status(forAsymmetryPercentage percentage: Double, age: Int) -> DashboardCardType.GaitStatus {
+        let thresholds: [Double] = asymmetryPercentageThresholdValue(age: age)
+        let normal = thresholds[1]  // 주어진 연령대에 해당하는 주의(Caution) 범위의 첫 번째 값 (lowerBound)
+        let caution = thresholds[2] // 주어진 연령대에 해당하는 주의(Caution) 범위의  마지막 값 (upperBound)
+
+        return evaluateStatusLessThan(percentage, normal: normal, caution: caution) // 전 연령 공통
     }
 
     ///
     func status(forDoubleSupportPercentage percentage: Double, age: Int) -> DashboardCardType.GaitStatus {
-        switch age {
-        case (..<20):   return evaluateStatusLessThan(percentage, normal: 0.20, caution: 0.25)   // 10대
-        case (20..<30): return evaluateStatusLessThan(percentage, normal: 0.20, caution: 0.25)   // 20대
-        case (30..<40): return evaluateStatusLessThan(percentage, normal: 0.22, caution: 0.28)   // 30대
-        case (40..<50): return evaluateStatusLessThan(percentage, normal: 0.24, caution: 0.30)   // 40대
-        case (50..<60): return evaluateStatusLessThan(percentage, normal: 0.26, caution: 0.32)   // 50대
-        case (60..<70): return evaluateStatusLessThan(percentage, normal: 0.28, caution: 0.34)   // 60대
-        default: return evaluateStatusLessThan(percentage, normal: 0.30, caution: 0.36)          // 70대
+        let thresholds: [Double] = doubleSupportPercentageThresholdValue(age: age)
+        let normal = thresholds[1]  // 주어진 연령대에 해당하는 주의(Caution) 범위의 첫 번째 값 (lowerBound)
+        let caution = thresholds[2] // 주어진 연령대에 해당하는 주의(Caution) 범위의  마지막 값 (upperBound)
+
+        return evaluateStatusLessThan(percentage, normal: normal, caution: caution)
+    }
+}
+
+extension DashboardCardType {
+
+    ///
+    func thresholdValues(age: Int) -> [Double] {
+        switch self {
+        case .walkingStepLength:                return stepLengthThresholdValues(age: age)
+        case .walkingAsymmetryPercentage:       return asymmetryPercentageThresholdValue(age: age)
+        case .walkingSpeed:                     return walkingSpeedThresholdValue(age: age)
+        case .walkingDoubleSupportPercentage:   return doubleSupportPercentageThresholdValue(age: age)
+        }
+    }
+
+    ///
+    func stepLengthThresholdValues(age: Int) -> [Double] {
+        switch age {  // higher is better
+        case (..<20):   return [45, 55, 65, 75] // 10대
+        case (20..<30): return [45, 55, 65, 75] // 20대
+        case (30..<40): return [43, 53, 63, 73] // 30대
+        case (40..<50): return [40, 50, 60, 70] // 40대
+        case (50..<60): return [38, 48, 58, 68] // 50대
+        case (60..<70): return [35, 45, 55, 65] // 60대
+        default: return [30, 40, 50, 60]        // 70대
+        }
+    }
+
+    ///
+    func walkingSpeedThresholdValue(age: Int) -> [Double] {
+        switch age {  // higher is better
+        case (..<20):   return [1.0, 1.2, 1.4, 1.6]     // 10대
+        case (20..<30): return [1.0, 1.2, 1.4, 1.6]     // 20대
+        case (30..<40): return [0.95, 1.15, 1.35, 1.55] // 30대
+        case (40..<50): return [0.9, 1.1, 1.3, 1.5]     // 40대
+        case (50..<60): return [0.75, 1.0, 1.25, 1.5]   // 50대
+        case (60..<70): return [0.7, 0.9, 1.1, 1.3]     // 60대
+        default: return [0.6, 0.8, 1.0, 1.2]            // 70대
+        }
+    }
+
+    ///
+    func asymmetryPercentageThresholdValue(age: Int) -> [Double] {
+        [0.01, 0.10, 0.19, 0.28] // lower is better
+    }
+
+    ///
+    func doubleSupportPercentageThresholdValue(age: Int) -> [Double] {
+        switch age { // lower is better
+        case (..<20):   return [0.15, 0.2, 0.25, 0.3]   // 10대
+        case (20..<30): return [0.15, 0.2, 0.25, 0.3]   // 20대
+        case (30..<40): return [0.16, 0.22, 0.28, 0.34] // 30대
+        case (40..<50): return [0.18, 0.24, 0.30, 0.36] // 40대
+        case (50..<60): return [0.2, 0.26, 0.32, 0.38]  // 50대
+        case (60..<70): return [0.22, 0.28, 0.34, 0.4]  // 60대
+        default: return [0.24, 0.3, 0.36, 0.42]         // 70대
         }
     }
 }
+
+extension DashboardCardType {
+
+    var higerIsBetter: Bool {
+        switch self {
+        case .walkingSpeed, .walkingStepLength:                            return true
+        case .walkingAsymmetryPercentage, .walkingDoubleSupportPercentage: return false
+        }
+    }
+}
+
 
 extension DashboardCardType {
 
@@ -144,6 +207,11 @@ extension DashboardCardType {
             case .caution:  return .systemYellow
             case .warning:  return .systemRed
             }
+        }
+
+        ///
+        var secondaryBackgroundColor: UIColor {
+            backgroundColor.withAlphaComponent(0.5)
         }
     }
 }
