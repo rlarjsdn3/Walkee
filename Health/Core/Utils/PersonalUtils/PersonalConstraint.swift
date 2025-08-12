7b478f75c1aecaf6d4717151e8deca0a8880f401
노기승 <juks8666@gmail.com>
2025-08-12 14:36:39 +0900

    MainActor 위치 수정

--- Health/Core/Utils/PersonalUtils/PersonalConstraint.swift
+++ Health/Core/Utils/PersonalUtils/PersonalConstraint.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 /// 기기와 방향에 따른 백그라운드 높이 계산 유틸리티
+@MainActor
 final class BackgroundHeightUtils {
 
     /// 기기와 방향에 따른 백그라운드 높이를 계산합니다
-    @MainActor static func calculateBackgroundHeight() -> CGFloat {
+    static func calculateBackgroundHeight() -> CGFloat {
         let screenHeight = UIScreen.main.bounds.height
         let screenWidth = UIScreen.main.bounds.width
 
@@ -34,7 +35,7 @@ final class BackgroundHeightUtils {
     }
 
     /// 제약 조건을 업데이트하고 애니메이션을 적용합니다
-    @MainActor static func updateBackgroundHeight(constraint: NSLayoutConstraint, in view: UIView) {
+    static func updateBackgroundHeight(constraint: NSLayoutConstraint, in view: UIView) {
         constraint.constant = calculateBackgroundHeight()
 
         UIView.animate(withDuration: 0.3) {
