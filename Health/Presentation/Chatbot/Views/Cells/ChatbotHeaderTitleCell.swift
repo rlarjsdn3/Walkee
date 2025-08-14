82460e9c291a1e8d027b547890c456ed89d23090
Seohyun Kim <cestbonciel88@knou.ac.kr>
2025-08-15 02:26:44 +0900

    fix: HeaderCell의 화면 닫기 버튼 추가

--- Health/Presentation/Chatbot/Views/Cells/ChatbotHeaderTitleCell.swift
+++ Health/Presentation/Chatbot/Views/Cells/ChatbotHeaderTitleCell.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 final class ChatbotHeaderTitleCell: CoreTableViewCell {
+	var onCloseTapped: (() -> Void)?
+	
 	private let chatbotImageView: UIImageView = {
 		let imageView = UIImageView()
 		imageView.image = UIImage(named: "chatBot")
@@ -26,6 +28,24 @@ final class ChatbotHeaderTitleCell: CoreTableViewCell {
 		return label
 	}()
 	
+	private let closeIconView: UIImageView = {
+		let buttonImage = UIImageView(image: UIImage(systemName: "xmark"))
+		buttonImage.translatesAutoresizingMaskIntoConstraints = false
+		buttonImage.contentMode = .scaleAspectFit
+		buttonImage.tintColor = .accent
+		return buttonImage
+	}()
+	
+	private lazy var closeButton: UIButton = {
+		let button = UIButton()
+		button.contentHorizontalAlignment = .center
+		button.contentVerticalAlignment   = .center
+		button.tintColor = .accent
+		button.addTarget(self, action: #selector(handleCloseButtonTapped), for: .touchUpInside)
+		button.translatesAutoresizingMaskIntoConstraints = false
+		return button
+	}()
+	
 	// MARK: - Initialization
 	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
 		super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -34,7 +54,6 @@ final class ChatbotHeaderTitleCell: CoreTableViewCell {
 			setupConstraints()
 			setupAttribute()
 		}
-		
 	}
 	
 	required init?(coder: NSCoder) {
@@ -52,29 +71,43 @@ final class ChatbotHeaderTitleCell: CoreTableViewCell {
 		
 		contentView.addSubview(chatbotImageView)
 		contentView.addSubview(welcomeLabel)
+		contentView.addSubview(closeButton)
+		closeButton.addSubview(closeIconView)
 	}
 	
 	override func setupConstraints() {
 		super.setupConstraints()
 		
+		let guide = contentView.safeAreaLayoutGuide
+		
 		NSLayoutConstraint.activate([
-			chatbotImageView.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-			chatbotImageView.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 0),
-			chatbotImageView.widthAnchor.constraint(equalToConstant: 48),
-			chatbotImageView.heightAnchor.constraint(equalToConstant: 48),
+			chatbotImageView.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: 16),
+			chatbotImageView.topAnchor.constraint(equalTo: guide.topAnchor),
+			chatbotImageView.widthAnchor.constraint(equalToConstant: 32),
+			chatbotImageView.heightAnchor.constraint(equalToConstant: 32),
 			
-			welcomeLabel.leadingAnchor.constraint(equalTo: chatbotImageView.trailingAnchor, constant: 16),
-			welcomeLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+			welcomeLabel.leadingAnchor.constraint(equalTo: chatbotImageView.trailingAnchor, constant: 12),
+			welcomeLabel.trailingAnchor.constraint(lessThanOrEqualTo: closeButton.leadingAnchor, constant: -12),
 			welcomeLabel.centerYAnchor.constraint(equalTo: chatbotImageView.centerYAnchor),
 			
-			contentView.bottomAnchor.constraint(greaterThanOrEqualTo: chatbotImageView.bottomAnchor, constant: 16),
-			contentView.bottomAnchor.constraint(greaterThanOrEqualTo: welcomeLabel.bottomAnchor, constant: 16)
+			closeButton.trailingAnchor.constraint(equalTo: guide.trailingAnchor, constant: -16),
+			closeButton.topAnchor.constraint(equalTo: chatbotImageView.topAnchor),
+			closeButton.widthAnchor.constraint(equalToConstant: 48),
+			closeButton.heightAnchor.constraint(equalToConstant: 48),
+			
+			contentView.bottomAnchor.constraint(greaterThanOrEqualTo: chatbotImageView.bottomAnchor, constant: 12),
+			contentView.bottomAnchor.constraint(greaterThanOrEqualTo: welcomeLabel.bottomAnchor, constant: 12),
+			contentView.bottomAnchor.constraint(greaterThanOrEqualTo: closeButton.bottomAnchor, constant: 12),
+			
+			closeIconView.centerXAnchor.constraint(equalTo: closeButton.centerXAnchor),
+			closeIconView.topAnchor.constraint(equalTo: chatbotImageView.topAnchor, constant: -2),
+			closeIconView.widthAnchor.constraint(equalToConstant: 32),
+			closeIconView.heightAnchor.constraint(equalTo: closeIconView.widthAnchor)
 		])
 	}
 	
 	override func setupAttribute() {
 		super.setupAttribute()
-		
 		selectionStyle = .none
 		backgroundColor = .clear
 	}
@@ -82,5 +115,10 @@ final class ChatbotHeaderTitleCell: CoreTableViewCell {
 	// MARK: - Configure Methods
 	func configure(with text: String) {
 		welcomeLabel.text = text
+	}
+	
+	// MARK: - Actions
+	@objc private func handleCloseButtonTapped() {
+		onCloseTapped?()
 	}
 }
